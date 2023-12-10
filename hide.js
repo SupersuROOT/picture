a42d05304b4cc9a9d7f768900b35d8e19a9cca94
SupersuROOT <77890922+SupersuROOT@users.noreply.github.com>
2023-12-10 10:08:34 +0800

    Update hide.js

--- hide.js
+++ hide.js
@@ -1,7 +1,7 @@
 /*******************************************
 
 注：此为本人自用的quantumult x重写脚本
-2023.12.10 1
+2023.12.10 3
 
 ********************************************
 
@@ -18,10 +18,13 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 if (obj.result && obj.result.user && obj.result.user.hasOwnProperty("name")) {
-    obj.result.user["name"] = "匿名";
+    obj.result.user["name"] = "none";
 }
 if (obj.result && obj.result.user && obj.result.user.userInfo && obj.result.user.userInfo.hasOwnProperty("loginName")) {
-    obj.result.user.userInfo["loginName"] = "edc20201314";
+    obj.result.user.userInfo["loginName"] = "edc202072630";
+}
+if (obj.result && obj.result.user && obj.result.user.userInfo && obj.result.user.userInfo.school && obj.result.user.userInfo.school.hasOwnProperty("schoolName")) {
+    obj.result.user.userInfo.school["schoolName"] = "石柱第一初级中学";
 }
 
 body = JSON.stringify(obj);
