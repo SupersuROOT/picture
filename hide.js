e8cdfcc0f3e4ab1f825d257491dd2dd344fbc1e6
SupersuROOT <77890922+SupersuROOT@users.noreply.github.com>
2023-12-10 10:00:39 +0800

    Update hide.js

--- hide.js
+++ hide.js
@@ -17,12 +17,13 @@ hostname = www.zhixue.com
 var body = $response.body;
 var obj = JSON.parse(body);
 
-if (obj.hasOwnProperty("name")) {
-    obj["name"] = "匿名";
+if (obj.result && obj.result.user && obj.result.user.hasOwnProperty("name")) {
+    obj.result.user["name"] = "匿名";
 }
-if (obj.hasOwnProperty("loginName")) {
-    obj["loginName"] = "edc20201314";
+if (obj.result && obj.result.user && obj.result.user.userInfo && obj.result.user.userInfo.hasOwnProperty("loginName")) {
+    obj.result.user.userInfo["loginName"] = "edc20201314";
 }
 
 body = JSON.stringify(obj);
 $done({body});
+
